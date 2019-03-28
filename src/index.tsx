5f8a112906971f77e7a9bed791df0253f564b312
Carlos Hernández Gil <chernandezgil@gmail.com>
2019-03-28 09:34:56 +0100

    add app structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#252934' },
+    secondary: { main: '#04C2C9' }
+  },
+  typography: {
+    htmlFontSize: 10,
+    useNextVariants: true,
+ 
+  }
+});
+
+
+
+
+
+
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </MuiThemeProvider>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

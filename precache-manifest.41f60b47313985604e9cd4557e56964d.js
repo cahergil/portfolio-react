d0d9cfbf43866fab47f792e39307e7c459d06a72
Carlos Hernández Gil <chernandezgil@gmail.com>
2019-04-09 16:42:44 +0200

    Updates

---
+++ precache-manifest.41f60b47313985604e9cd4557e56964d.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "07affe68cc105ca42e2f47596b967006",
+    "url": "/portfolio-react/static/media/feed_reader.07affe68.png"
+  },
+  {
+    "revision": "b75ef9e81e0f54f588fc",
+    "url": "/portfolio-react/static/css/main.6ba1b294.chunk.css"
+  },
+  {
+    "revision": "11528ddda4154ce1e32b",
+    "url": "/portfolio-react/static/js/runtime~main.72b3206a.js"
+  },
+  {
+    "revision": "e58b10bed51172917a370da08df3fc31",
+    "url": "/portfolio-react/static/media/twitter.e58b10be.svg"
+  },
+  {
+    "revision": "4d19dce06ef73b77ef18",
+    "url": "/portfolio-react/static/js/2.cfd95f57.chunk.js"
+  },
+  {
+    "revision": "d8541b7e0b507635351758283281f526",
+    "url": "/portfolio-react/static/media/carnet_clipped.d8541b7e.jpg"
+  },
+  {
+    "revision": "9512477b5191e76d020c954314b28b71",
+    "url": "/portfolio-react/static/media/desktop.9512477b.jpg"
+  },
+  {
+    "revision": "77ce1914b109486e4409e870e1ab260b",
+    "url": "/portfolio-react/static/media/skills__optimized.77ce1914.jpg"
+  },
+  {
+    "revision": "d262f5ef0b863cffcf115b688724c66f",
+    "url": "/portfolio-react/static/media/location.d262f5ef.jpg"
+  },
+  {
+    "revision": "6639f5afc3fa9b1a0f5d389f01f4c3c6",
+    "url": "/portfolio-react/static/media/arcade-game.6639f5af.png"
+  },
+  {
+    "revision": "23275e814a166cf391f1ade303c2fa5b",
+    "url": "/portfolio-react/static/media/farmacias1.23275e81.png"
+  },
+  {
+    "revision": "b75ef9e81e0f54f588fc",
+    "url": "/portfolio-react/static/js/main.83db5b0f.chunk.js"
+  },
+  {
+    "revision": "3f70ed57e8fea0bf10b84f14b0a0bf2f",
+    "url": "/portfolio-react/static/media/github.3f70ed57.svg"
+  },
+  {
+    "revision": "6f1e3677f981891ec62bbffd9d22a8f1",
+    "url": "/portfolio-react/static/media/mg.6f1e3677.png"
+  },
+  {
+    "revision": "5a767a6979feb66472cdc7c7ec7158f5",
+    "url": "/portfolio-react/static/media/myreads.5a767a69.png"
+  },
+  {
+    "revision": "abb2206dd4e07c6c7f1fa6ab09887a53",
+    "url": "/portfolio-react/static/media/neighborhood_small.abb2206d.png"
+  },
+  {
+    "revision": "b5571daa613006087c20371116ced720",
+    "url": "/portfolio-react/static/media/portfolio.b5571daa.png"
+  },
+  {
+    "revision": "ca28c377e164853b27f296687060f38b",
+    "url": "/portfolio-react/static/media/portfolio_react.ca28c377.png"
+  },
+  {
+    "revision": "3b06186df892415efbc11a9e21930d4f",
+    "url": "/portfolio-react/static/media/restaurants_review.3b06186d.png"
+  },
+  {
+    "revision": "4df936190fd5aac0ac00a1db56718460",
+    "url": "/portfolio-react/static/media/skills.4df93619.jpg"
+  },
+  {
+    "revision": "b0ff32ab8cd5a92f77626117c0b80d2b",
+    "url": "/portfolio-react/static/media/symbol-defs.b0ff32ab.svg"
+  },
+  {
+    "revision": "4d19dce06ef73b77ef18",
+    "url": "/portfolio-react/static/css/2.54c47512.chunk.css"
+  },
+  {
+    "revision": "bd2b146a6719a50d028fd4aa0335efd5",
+    "url": "/portfolio-react/index.html"
+  }
+];
